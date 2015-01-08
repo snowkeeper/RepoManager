a9c791f19bef073bfa2101ac5565039b47b6ccde
Snowkeeper <snowkeeper@snowpi.org>
2015-01-08 08:34:49 -0500

    nightly

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-var RepoMan = require('./lib');
+var RepoMan = require('repoman');
+
+
+RepoMan.init('keystone');
 
 
 
-RepoMan.init({
-	//pkg: './repoman.json'
+RepoMan.on('repo list',function(obj) {
+	console.log('repo list loaded');
 });
-
-RepoMan.on('pkg file',function(obj) {
-	console.log('package',obj);
-});
-RepoMan.on('pkg file error',function(error) {
+RepoMan.on('pkg get file error',function(error) {
 	console.log('pkg error',error);
+});
+RepoMan.on('repo add',function(msg) {
+	console.log('repo add',msg);
+});
+RepoMan.on('repo add error',function(error) {
+	console.log('repo add error',error);
+});
+
+RepoMan.on('ready',function() {
+	console.log('ready');
+	//RepoMan.add('keystone','/home/snow/projects/github/keystone');
+	//RepoMan.add('keystone2','/home/snow/projects/github/keystone2');
+	//RepoMan.use('test');
+});
+
+RepoMan.on('use repo',function(repo) {
+	console.log('use repo',repo);
 });
 
